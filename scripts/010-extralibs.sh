19956ff610c4a0a2fef7c4cb9b1a75c44795fbce
rhynec <rhynec@pm.me>
2022-02-12 18:14:27 -0600

    Bump extralibs version
    - Add ps3mapi and libtre-ps3 to extralibs.
    - Remove broken docker workflow.

--- scripts/010-extralibs.sh
+++ scripts/010-extralibs.sh
@@ -2,7 +2,7 @@
 # ps3libraries.sh by Naomi Peori (naomi@peori.ca)
 
 ## Download the source code.
-wget --no-check-certificate https://github.com/PS3SDK-Misc/extralibs/archive/00b0d99b279af89fd2c59d9ab98fad99c8fbf677.tar.gz -O extralibs.tar.gz
+wget --no-check-certificate https://github.com/PS3SDK-Misc/extralibs/archive/fbbb3111d481edd528c654619e1298d48227063c.tar.gz -O extralibs.tar.gz
 
 ## Unpack the source code.
 rm -Rf extralibs && mkdir extralibs && tar --strip-components=1 --directory=extralibs -xvzf extralibs.tar.gz && cd extralibs
