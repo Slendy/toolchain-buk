2554edd3bde9ef60cc6c5066ec88a3dd59588e7c
rhynec <86078431+rhynec@users.noreply.github.com>
2022-02-12 19:15:44 -0600

    Bump extralibs version
    - New version fixes building libtre-ps3.

--- scripts/010-extralibs.sh
+++ scripts/010-extralibs.sh
@@ -2,7 +2,7 @@
 # ps3libraries.sh by Naomi Peori (naomi@peori.ca)
 
 ## Download the source code.
-wget --no-check-certificate https://github.com/PS3SDK-Misc/extralibs/archive/fbbb3111d481edd528c654619e1298d48227063c.tar.gz -O extralibs.tar.gz
+wget --no-check-certificate https://github.com/PS3SDK-Misc/extralibs/archive/a268ac81834fa6cb1310e3026a58d28f61376cf7.tar.gz -O extralibs.tar.gz
 
 ## Unpack the source code.
 rm -Rf extralibs && mkdir extralibs && tar --strip-components=1 --directory=extralibs -xvzf extralibs.tar.gz && cd extralibs
